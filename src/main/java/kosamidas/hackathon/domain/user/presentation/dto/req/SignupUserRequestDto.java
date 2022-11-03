c6030dc9c72d3f4d23c5bb012df39cd76fb41998
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-11-03 16:34:25 +0900

    remove :: 회원가입 validate @NotBlank 통일

--- src/main/java/kosamidas/hackathon/domain/user/presentation/dto/req/SignupUserRequestDto.java
+++ src/main/java/kosamidas/hackathon/domain/user/presentation/dto/req/SignupUserRequestDto.java
@@ -15,18 +15,17 @@ import javax.validation.constraints.Pattern;
 @Getter
 public class SignupUserRequestDto {
 
-    @NotBlank(message = "이메일은 필수 입력 값입니다.")
-    @Email(message = "이메일 형식에 맞지 않습니다.")
+    @NotBlank(message = "아이디 필수 입력 값입니다.")
     private final String authId;
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
     private final String name;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private final String password;
+    @NotBlank(message = "비밀번호 확인 값은 필수 입력 값입니다.")
     private final String checkPassword;
+    @NotBlank(message = "부서는 필수 입력 값입니다.")
     private final String department;
     private final String Authority;
 
