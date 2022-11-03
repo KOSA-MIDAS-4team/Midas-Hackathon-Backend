18e23ee72458bb29b04fc8c3b0be9d7338d9481c
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-11-03 23:44:36 +0900

    fix :: 날짜 입력 받아서 그 날의 출근/퇴근 시간 및 근로 시간 조회 findFirst에 다걸러지는 문제

--- src/main/java/kosamidas/hackathon/domain/user/service/UserService.java
+++ src/main/java/kosamidas/hackathon/domain/user/service/UserService.java
@@ -79,13 +79,33 @@ public class UserService {
 
     public UserCommuteDateResponseDto getDateInfo(LocalDate date) {
         User user = userFacade.getCurrentUser();
-        Optional<Commute> commuteOptional = commuteFacade.findAll()
+        List<Commute> commutes = commuteFacade.findAll()
                 .stream()
-                .findFirst()
-                .filter(commute -> commute.getUser().getId().equals(user.getId()))
-                .filter(commute -> date.getYear() == commute.getOfficeWentDate().getYear())
-                .filter(commute -> date.getMonthValue() == commute.getOfficeWentDate().getMonthValue())
-                .filter(commute -> date.getDayOfMonth() == commute.getOfficeWentDate().getDayOfMonth());
-        return commuteOptional.map(UserCommuteDateResponseDto::new).orElse(null);
+                .filter(commute -> {
+                    System.out.println("commute.getUser().getId() = " + commute.getUser().getId());
+                    System.out.println("currentUser = " + userFacade.getCurrentUser().getId());
+                    return commute.getUser().getId().equals(user.getId());
+                })
+                .filter(commute -> {
+                    System.out.println("date.getYear() = " + date.getYear());
+                    System.out.println("commute = " + commute.getOfficeWentDate().getYear());
+                    return date.getYear() == commute.getOfficeWentDate().getYear();
+                })
+                .filter(commute -> {
+                    System.out.println("date.getMonthValue() = " + date.getMonthValue());
+                    System.out.println("commute.getOfficeWentDate().getMonthValue() = " + commute.getOfficeWentDate().getMonthValue());
+                    return date.getMonthValue() == commute.getOfficeWentDate().getMonthValue();
+                })
+                .filter(commute -> {
+                    System.out.println("date.getDayOfMonth() = " + date.getDayOfMonth());
+                    System.out.println("commute = " + commute.getOfficeWentDate().getDayOfMonth());
+                    return date.getDayOfMonth() == commute.getOfficeWentDate().getDayOfMonth();
+                })
+                .collect(Collectors.toList());
+        if (!commutes.isEmpty()) {
+            return new UserCommuteDateResponseDto(commutes.get(0));
+        } else {
+            return null;
+        }
     }
 }
